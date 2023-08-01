b9da12010412a08b2c2a41bad002ccabe2b05ee0
ptipitaka <contact@ptipitaka.org>
2023-08-01 13:16:54 +0700

    modify Structure model

--- tipitaka/models.py
+++ tipitaka/models.py
@@ -192,17 +192,14 @@ class TableOfContent(models.Model):
 
 
 class Structure(MPTTModel):
-    table_of_content = models.ForeignKey(TableOfContent, verbose_name=_("Table of contents"), on_delete=models.CASCADE)
+    table_of_content = models.ForeignKey(TableOfContent, on_delete=models.CASCADE, verbose_name=_("Table of contents"),)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     code = models.CharField(max_length=20, null=True, blank=True, verbose_name=_("Code"))
-    title = models.CharField(verbose_name=_("Title"), db_index=True, max_length=255)
-    description = models.TextField(verbose_name=_("Description") ,max_length=255, blank=True, null=True)
-    ro = models.CharField(verbose_name=_("Roman Script"), null=True, blank=True, max_length=255)
-    si = models.CharField(verbose_name=_("Sinhala Script"), null=True, blank=True, max_length=255)
-    hi = models.CharField(verbose_name=_("Hindi Script"), null=True, blank=True, max_length=255)
-    lo = models.CharField(verbose_name=_("Lao Script"), null=True, blank=True, max_length=255)
-    my = models.CharField(verbose_name=_("Myanmar Script"), null=True, blank=True, max_length=255)
-    km = models.CharField(verbose_name=_("Khmar Script"), null=True, blank=True, max_length=255)
+    title_number = models.CharField(max_length=20, null=True, blank=True, verbose_name=_("Number")) 
+    title = models.CharField(max_length=255, db_index=True, verbose_name=_("Title"))
+    additional_title = models.CharField(max_length=255, blank=True, null=True, verbose_name=_("Additional Title"))
+    description = models.TextField(max_length=255, blank=True, null=True, verbose_name=_("Description") )
+    ro = models.CharField(max_length=255, null=True, blank=True, verbose_name=_("Roman Script"))
 
     class MPTTMeta:
         pass
